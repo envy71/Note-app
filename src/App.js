ea9632134c91f53b1f3a3f30b68224a01a3fa2cf
Nickolas Vu <nickvu71@gmail.com>
2022-06-14 01:15:56 +0000

    bug fix with delete

--- src/App.js
+++ src/App.js
@@ -50,7 +50,7 @@ class App extends Component {
   }
 
   deleteNote = (id) => {
-    const newNotesState = this.state.notes.filter((note) => note.id ! == id);
+    const newNotesState = this.state.notes.filter((note) => note.id !== id );
     axios.delete(urlFor(`notes\${id}`))
     .then((res) => this.setState({ notes: newNotesState }))
     .catch((err) => console.log(err.response.data) );
